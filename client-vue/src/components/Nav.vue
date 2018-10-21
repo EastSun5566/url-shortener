64e9bfde4de3144d5d88602e589bf3a2c9f53beb
汪東陽 <michael19920327@gmail.com>
2018-10-21 18:26:51 +0800

    🔧 修改前端畫面

---
+++ client-vue/src/components/Nav.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <router-link
+      class="navbar-brand"
+      to="/">ㄏㄏ</router-link>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarColor01"
+      aria-controls="navbarColor01"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"/>
+    </button>
+
+    <div
+      id="navbarColor01"
+      class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a
+            class="nav-link"
+            href="https://github.com/EastSun5566"
+            target="_blank">
+            <i class="fab fa-github"/>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://codepen.io/EastSun5566/"
+            target="_blank">
+            <i class="fab fa-codepen"/>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ client-vue/src/views/Index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-3">東陽 a 煞氣短網址 🔥</h1>
+      <p class="lead">保證很短 der，馬上來體驗 👇</p>
+
+      <hr class="my-4">
+
+      <form
+        class="lead"
+        @submit.prevent="getShortUrl">
+
+        <div class="form-group">
+          <label
+            class="col-form-label col-form-label-lg"
+            for="inputLarge">🔥 想要縮 der 網址</label>
+          <input
+            id="inputLarge"
+            v-model="link.originalUrl"
+            class="form-control form-control-lg"
+            type="text"
+            placeholder="例如： https://github.com/EastSun5566">
+        </div>
+
+        <div class="form-group">
+          <label
+            class="col-form-label col-form-label-lg"
+            for="inputLarge">🔥 想要客製化 der 路徑</label>
+          <input
+            id="inputLarge"
+            v-model="link.customizedPath"
+            class="form-control form-control-lg"
+            type="text"
+            placeholder="例如： chill-out">
+        </div>
+
+        <button
+          type="submit"
+          class="btn btn-primary btn-lg">GOGO 🚀</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      link: {
+        originalUrl: '',
+        customizedPath: '',
+      },
+    };
+  },
+  methods: {
+    getShortUrl() {
+      console.log(this.link);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.jumbotron {
+  min-height: 100vh;
+  margin-bottom: 0;
+  padding-top: 160px;
+
+  background: #c9d6ff; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #e2e2e2,
+    #c9d6ff
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #e2e2e2,
+    #c9d6ff
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
+
