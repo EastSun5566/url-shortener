814eea9ab9d9f9b16ec9a9a1224e85d8f804b9ab
汪東陽 <michael19920327@gmail.com>
2018-10-25 01:12:08 +0800

    🔧 修改畫面

---
+++ client-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.scss";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>123</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
